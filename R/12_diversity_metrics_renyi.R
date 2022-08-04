c87ff4408bdf5a2cc9b90487673218488a04903b
JSMendivelso <jsmendivelsor@unal.edu.co>
2022-08-04 13:08:29 -0300

    Adding script for Rényi diversity

---
+++ R/12_diversity_metrics_renyi.R
@@ -0,0 +1,27 @@
+#August 3/2022
+#Understanding shich community is more diverse is not always straigtforward
+
+Community.A <- c(10,6,4,1)
+Community.B <- c(17, rep(1,7)) #rep replicates elements of vectors and lists.
+
+library(vegan)
+
+diversity(Community.A, "shannon")
+diversity(Community.B, "shannon")
+diversity(Community.A, "invsimpson")
+diversity(Community.B, "invsimpson")
+
+ren_comA <- renyi(Community.A)
+ren_comB <- renyi(Community.B)
+
+ren_AB <- rbind(ren_comA,ren_comB)
+matplot(t(ren_AB)) #Matoplot is made for matrices. Plot the columns of one matrix against the columns of another. t command is the tranpose
+matplot(t(ren_AB), ylab = "Rényi diversity", type = 'l', axes = F) #Using lines in the plot and eliminating axes
+box() #Adding box to the plot
+axis(side=2)
+axis(side=1, labels = c(0,0.25,0.5,1,2,4,8,16,32,64,"Inf"), at=1:11)
+
+legend("topright",
+       legend = c("Community A", "Community B"), lty=c(1,2),col=c(1,2))
+
+ren_comB_Hill <- renyi(Community.B, hill = T)
