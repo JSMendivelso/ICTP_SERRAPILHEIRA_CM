633219a1a6a7c1053afdb0b1c25093eb6bb5798d
JSMendivelso <jsmendivelsor@unal.edu.co>
2022-08-17 01:29:42 -0300

    Implementing good practices

--- R/07_ODEs.R
+++ R/07_ODEs.R
@@ -1,4 +1,12 @@
-library(deSolve) #because wi will solve ODE
+# --------------------------------------------------#
+# Scientific computing
+# ICTP/Serrapilheira 2022
+# Implementing population models
+# First Version 2022-07-21
+# --------------------------------------------------#
+
+# Loading needed packages
+library(deSolve) #because we will solve ODE
 library(ggplot2) # because we will plot things
 library(tidyr) # because we will manipulate some data
 
@@ -13,22 +21,22 @@ source("./logGrowth.R")
 #  })
 #}
 #We will specify the parameters and then solve the ODE
-p<-c(r=1,a=0.001) #Named vector with parameters
-y0<-c(N=10)  #Initial condition
-t<-1:20 #Time steps
+p <- c(r = 1, a = 0.001) #Named vector with parameters
+y0 <- c(N = 10)  #Initial condition
+t <- 1:20 #Time steps
 #We can use ode() to solve the equation
-out_log<-ode(y=y0,times=t,func=logGrowth,parms=p)
+out_log <- ode(y = y0, times = t, func = logGrowth, parms = p)
 #Plotting
-df_log<-as.data.frame(out_log)
+df_log <- as.data.frame(out_log)
 ggplot(df_log) +
-  geom_line(aes(x=time,y=N))+
+  geom_line(aes(x = time, y = N))+
   theme_classic()
 
 #LOKTA-VOLTERRA COMPETITION MODEL
 
-LVComp<-function(t,y,p){
-  N<-y
-  with(as.list(p),{
+LVComp <- function(t,y,p){
+  N <- y
+  with(as.list(p), {
     dN1.dt <- r[1] * N[1] * (1 - a[1, 1] * N[1] - a[1, 2] * N[2])
     dN2.dt <- r[2] * N[2] * (1 - a[2, 1] * N[1] - a[2, 2] * N[2])
     return(list(c(dN1.dt, dN2.dt)))
@@ -37,16 +45,16 @@ LVComp<-function(t,y,p){
 #Solving the system. For this particular case, we need to define
 #the α matrix with the competition coefficients
 
-a<-matrix(c(0.02,0.01,0.01,0.03),nrow=2)
-r<-c(1,1)
-p2<-list(r,a)
-N0<-c(10,10)
-t2<-c(1:100)
+a <- matrix(c(0.02, 0.01, 0.01, 0.03), nrow = 2)
+r <- c(1, 1)
+p2 <- list(r, a)
+N0 <- c(10, 10)
+t2 <- c(1:100)
 
-out_lv<-ode(y=N0,times=t2,func=LVComp,parms=p2)
-df_lv<-pivot_longer(as.data.frame(out_lv),cols=2:3) #from tidyr to organize the information as we want
-                                                    #cols: columns that I want to colapse
+out_lv <- ode(y = N0, times = t2, func = LVComp, parms = p2)
+df_lv <- pivot_longer(as.data.frame(out_lv), cols = 2:3) #from tidyr to organize the information as we want
+                                                    #cols: columns that I want to collapse
 ggplot(df_lv) +
-  geom_line(aes(x=time,y=value,color=name))+
-  labs(x='Time',y="N",color="Species")+
+  geom_line(aes(x = time, y = value, color = name)) +
+  labs(x = 'Time', y = "N", color = "Species")+
   theme_classic()
