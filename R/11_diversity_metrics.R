156476de1305389274f8d32906bb166b844d5268
JSMendivelso <jsmendivelsor@unal.edu.co>
2022-08-21 22:42:26 -0300

    Adding simpson and inverse simpson diversity functions

--- R/11_diversity_metrics.R
+++ R/11_diversity_metrics.R
@@ -9,7 +9,7 @@ abund <- mapply(sum,comm[,-1])
 most_abund <- sort(abund,decreasing=T)
 
 #richness=species/site
-#site=columnas
+#site=columns
 
 #Turning into binary to calculate richness
 comm2<-comm
@@ -29,6 +29,61 @@ for (i in 1:nrow(comm)){
   most_abundant_site <- c(most_abundant_site,index)
 }
 
+#---CREATING FUNCTIONS FOR SHANNON AND SIMPSON DIVERSITY IN COMM DATASET
+#---------------Shannon
 
+
+ShannonCalc <- function (x){
+  ShannonFila <- c()
+  for (i in 1:nrow(x)){
+    fila <- x[i,]
+    totalFila <- sum(fila)
+    nozero <- fila[fila!=0]
+    multi <- (nozero/totalFila)*log(nozero/totalFila)
+    sumita <- sum(multi)
+    ShannonFila[i] <- -sumita
+  }
+  return(ShannonFila)
+}
+commNum <- comm[,2:57]
+ShannonCalc (commNum)
+comm$H <- ShannonCalc (commNum)
+View(comm)
+#Testing
+diversity (commNum, "shannon")
+
+#---------------------------Simpson
+SimpsonCalc <- function (x){
+  SimpsonFila <- c()
+  for (i in 1:nrow(x)){
+    fila <- x[i,]
+    totalFila <- sum(fila)
+    prob <- fila/totalFila
+    elevado <- prob^2
+    sumita <- sum(elevado)
+    SimpsonFila[i] <- 1-sumita}
+  return(SimpsonFila)
+}
+
+comm$Simp <- SimpsonCalc (commNum)
+View(comm)
+#Testing
+diversity (commNum, "simpson")
+
+#-----------------InvSimpson
+InvSimpsonCalc <- function (x){
+  InvSimpsonFila <- c()
+  for (i in 1:nrow(x)){
+    fila <- x[i,]
+    totalFila <- sum(fila)
+    prob <- fila/totalFila
+    elevado <- prob^2
+    sumita <- sum(elevado)
+    InvSimpsonFila[i] <- 1/sumita}
+  return(InvSimpsonFila)
+}
+
+comm$InvSimp <- InvSimpsonCalc (commNum)
+diversity (commNum, "invsimpson")
 
 
