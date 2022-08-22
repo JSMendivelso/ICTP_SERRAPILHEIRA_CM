ed730c2e9607035cfdc261af644deaa9113bfcad
JSMendivelso <jsmendivelsor@unal.edu.co>
2022-08-21 22:41:43 -0300

    Fixing spelling

--- R/12_diversity_metrics_renyi.R
+++ R/12_diversity_metrics_renyi.R
@@ -1,5 +1,5 @@
 #August 3/2022
-#Understanding shich community is more diverse is not always straigtforward
+#Understanding which community is more diverse is not always straightforward
 
 Community.A <- c(10,6,4,1)
 Community.B <- c(17, rep(1,7)) #rep replicates elements of vectors and lists.
@@ -15,7 +15,7 @@ ren_comA <- renyi(Community.A)
 ren_comB <- renyi(Community.B)
 
 ren_AB <- rbind(ren_comA,ren_comB)
-matplot(t(ren_AB)) #Matoplot is made for matrices. Plot the columns of one matrix against the columns of another. t command is the tranpose
+matplot(t(ren_AB)) #Matoplot is made for matrices. Plot the columns of one matrix against the columns of another. t command is the transpose
 matplot(t(ren_AB), ylab = "Rényi diversity", type = 'l', axes = F) #Using lines in the plot and eliminating axes
 box() #Adding box to the plot
 axis(side=2)
